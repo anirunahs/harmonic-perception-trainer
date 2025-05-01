8e5d20b6db1193b7e43ca7f35acc207040671d9a
anirunahs <anirunahs@gmail.com>
2025-05-01 12:02:30 +0300

    feat(dataset): add script to convert Minor chord audio files to spectrogram images

--- models/audio_to_spectrogram.py
+++ models/audio_to_spectrogram.py
@@ -25,3 +25,23 @@ for file in MajorAudioFiles:
     cv2.imwrite(Path_for_Major_Spectrogram+"/"+filename+".png", ImageAudio)
     print(filename)
 
+# Minor files
+Path_for_Minor_Spectrogram="../../MICC/Minor"
+isExist = os.path.exists(Path_for_Minor_Spectrogram)
+if not isExist:
+    os.makedirs(Path_for_Minor_Spectrogram)
+    print("The new Minor folder created!")
+
+MinorAudioFiles = glob("../../musical-instrument-chord-classification/Minor/*.wav")
+
+for file in MinorAudioFiles:
+    y, sr = librosa.load(file)
+    D = librosa.stft(y)
+    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+    ImageAudio = (S_db * 255).astype(np.uint8)
+
+    idx = file.rfind("\\")
+    filename = file[idx+1:]
+
+    cv2.imwrite(Path_for_Minor_Spectrogram+"/"+filename+".png", ImageAudio)
+    print(filename)
