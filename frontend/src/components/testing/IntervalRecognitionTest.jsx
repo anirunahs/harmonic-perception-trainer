5a0e0a716b06f25977966a32b33c02a6bb8b1fcd
anirunahs <anirunahs@gmail.com>
2025-06-09 19:20:35 +0300

    feat(testing-page): add testing page with test type selection, settings UI, and route

---
+++ frontend/src/components/testing/IntervalRecognitionTest.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const IntervalRecognitionTest = () => {
+
+    return (
+        <>
+      <div>Test1</div>
+    </>
+    );
+
+};
+
+export default IntervalRecognitionTest;
