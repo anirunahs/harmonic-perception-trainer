afe43bcc5bd1f9410793597889e6fb2a293c586b
anirunahs <anirunahs@gmail.com>
2026-01-21 05:58:52 +0200

    chore(recognition): create recognition module structure

---
+++ backend/core/recognition/__init__.py
@@ -0,0 +1,6 @@
+"""Recognition Module - Audio processing facade for interval recognition."""
+
+from .result import RecognitionResult
+from .facade import AudioProcessingFacade
+
+__all__ = ['RecognitionResult', 'AudioProcessingFacade']
